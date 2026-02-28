230fe7e41b2b2cdc11d9302136732b31394c7260
SillyKitten1337 <119003324+SpyrosTatsis@users.noreply.github.com>
2026-02-28 17:46:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.getElementById("removeBtn").addEventListener("click", function() {
-
-    document.getElementById("container").remove();
-
+    document.getElementById("container").style.display = "none";
     document.body.classList.add("showBackground");
-
     document.getElementById("musicPlayer").classList.add("visible");
-
     document.getElementById("audioPlayer").play();
 
+    document.getElementById("quizBtn").style.display = "block";
+});
+
+
+document.getElementById("quizBtn").addEventListener("click", function() {
+    window.location.href = "/quiz";
 });
